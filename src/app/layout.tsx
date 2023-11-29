16986463e2b3254f66c5395797605634cc25ba3d
alexeykiruhin <q23sa3501080@gmail.com>
2023-11-29 15:08:18 +0300

    Add Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+'use client'
 import type {Metadata} from 'next'
+import {Layout} from "antd";
 import {Inter} from 'next/font/google'
 import './globals.css'
 import Link from 'next/link';
@@ -6,16 +8,16 @@ import {router} from 'next/client';
 
 const inter = Inter({subsets: ['latin']})
 
-export const metadata: Metadata = {
-    title: 'Project "e"',
-    description: 'Created by Aleksey Kiryukhin',
-}
+// export const metadata: Metadata = {
+//     title: 'Project "e"',
+//     description: 'Created by Aleksey Kiryukhin',
+// }
 const navigation = [
     {name: 'Home', href: '/', current: true},
     {name: 'Dashboard', href: '/dashboard', current: false},
     {name: 'Rating', href: '/rating', current: false},
 ]
-
+const {Header} = Layout;
 export default function RootLayout({
                                        children,
                                    }: {
@@ -24,6 +26,22 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body className={inter.className}>
+        <div className={'Header'}>
+            <Header
+            style={{
+                position: 'sticky',
+                top: 0,
+                zIndex: 1,
+                width: '100%',
+                display: 'flex',
+                alignItems: 'center',
+                color: 'white'
+            }}
+            // color='light'
+        >
+            Header
+        </Header>
+        </div>
         <div className={'NavBar'}>
             {navigation.map((item) => (
                 <Link
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ export default function Home() {
 
     return (
         <div className="Home">
-            <h2>Home </h2>
+            <h2>Home</h2>
+
         </div>
     )
 }
